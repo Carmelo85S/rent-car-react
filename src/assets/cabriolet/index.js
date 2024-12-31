78efc46022753ceb17586448dd0d67f14493ce35
Carmelo85S <carmelo.salis85@gmail.com>
2024-12-31 10:04:42 +0100

    deleted maserati from cabriolet -> index.js

--- src/assets/cabriolet/index.js
+++ src/assets/cabriolet/index.js
@@ -3,6 +3,5 @@ import Porsche from './porsche-911.webp';
 import BmwS8 from './bmw-8.webp';
 import BmwZ4 from './bmw-z4.webp';
 import Mustang from './ford-mustang.webp';
-import Maserati from './Maserati.webp';
 
-export { AstonMartin, Porsche, BmwS8, BmwZ4, Mustang, Maserati };
+export { AstonMartin, Porsche, BmwS8, BmwZ4, Mustang };
